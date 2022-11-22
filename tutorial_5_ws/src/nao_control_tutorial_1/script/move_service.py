6f2e81157b960c54461dd68d58ddfc1fe1a04226
Yi Zhang <ge89vod@mytum.de>
2022-11-22 14:07:21 +0100

    complete service

--- tutorial_5_ws/src/nao_control_tutorial_1/script/move_service.py
+++ tutorial_5_ws/src/nao_control_tutorial_1/script/move_service.py
@@ -9,46 +9,25 @@ motionProxy =0
 
 #TODO: create service handler
 def handle_move_joints(req):
-    #Exercise 1
-    motionProxy.setStiffnesses("RArm" ,1.0)
-    names = 'RShoulderRoll'
-    angles = 30.0*almath.TO_RAD
-    fractionMaxSpeed = 0.1
-    motionProxy.setAngles(names,angles,fractionMaxSpeed)
-    motionProxy.setStiffnesses("RArm", 0.0)
+    names = req.names
+    angles = req.angles
+    fractionMaxSpeed = req.speed
+    timeLists = req.time_second
+    isAbsolute = true
 
+    if(req.mode == 1):
+        motionProxy.setStiffnesses("Body" ,1.0)
+        motionProxy.setAngles(names,angles,fractionMaxSpeed)
+        motionProxy.setStiffnesses("Body", 0.0)
+    else if(req.mode == 2):
+        motionProxy.setStiffnesses("Body" ,1.0)
+        motionProxy.angleInterpolation(names,angles,fractionMaxSpeed,isAbsolute)
+        motionProxy.setStiffnesses("Body", 0.0)
+        taskList = motionProxy.getTaskList()
+        uiMotion = taskList[0][1]
+        motionProxy.killTask(uiMotion)
 
-    #Exercise 2
-    motionProxy.setStiffnesses("LArm" ,1.0)
-    names = AL::ALValue::array("LShoulderRoll", "LShoulderPitch")
-    angles = [30.0*almath.TO_RAD, 0.0*almath.TO_RAD]
-    timeLists = [2.0, 1.]
-    bool isAbsolute = true;
-    motionProxy.angleInterpolation(names,angles,fractionMaxSpeed,isAbsolute)
-    motionProxy.setStiffnesses("LArm", 0.0)
-
-
-
-    # if req.id==1:
-    #     for i in range(10):
-    #         motionProxy.setStiffnesses("Body" ,1.0)
-    #     motionProxy.setAngles(req.name,req.angle,req.speed)
-    #     print("Ready")
-    # if req.id==2:        
-    #     for i in range(30):
-    #         motionProxy.setStiffnesses("Body" ,1.0)
-    #     name = req.name
-    #     angle = req.angle
-    #     time=req.time
-    #     isAbsolute=True
-    #     motionProxy.angleInterpolation(name,angle,time,isAbsolute)
-    #     list = motionProxy.getTaskList()
-
-    #     if len(list) == 1:
-    #         motionProxy.killTask(list[0][1])
-    #     if len(list) == 2:
-    #         motionProxy.killTask(list[1][1])
-    #     print("Ready to move with interpolation")
+    return MoveJointsResponse(true)
 
 
 if __name__ == '__main__':
