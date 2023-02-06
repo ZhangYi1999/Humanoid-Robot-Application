848c8312210113ade65d7c7b7bf112ff343483f2
Yinglei Song <yinglei.song@tum.de>
2023-02-06 11:43:22 +0100

    face

--- project/src/face/src/face_detect.py
+++ project/src/face/src/face_detect.py
@@ -26,38 +26,11 @@ class face_detect:
         self.name_msg.data.append(0)
         self.name_msg.data.append(0)
         self.name_msg.data.append(0)
+
+        self.count = 0
+        self.face_history = np.zeros((5,4), dtype=int)
         
         rospy.loginfo("Initialize face detection.")
-
-        # # create path for images
-        # self.dir_path = os.path.dirname(os.path.realpath(__file__))
-        # self.pkg_path = os.path.dirname(self.dir_path)
-        # self.syl_path = os.path.join(self.pkg_path, 'face_img/syl.jpeg')
-        # self.zy_path = os.path.join(self.pkg_path, 'face_img/zy.jpeg')
-        # self.zcy_path = os.path.join(self.pkg_path, 'face_img/zcy.jpeg')
-
-        # rospy.loginfo("Create path.")
-        # # load images and detect the faces, save face encodings
-        # self.syl_image = face_recognition.load_image_file(self.syl_path)
-        # self.syl_face_encoding = face_recognition.face_encodings(self.syl_image)[0]
-        # self.zy_image = face_recognition.load_image_file(self.zy_path)
-        # self.zy_face_encoding = face_recognition.face_encodings(self.zy_image)[0]
-        # self.zcy_image = face_recognition.load_image_file(self.zcy_path)
-        # self.zcy_face_encoding = face_recognition.face_encodings(self.zcy_image)[0]
-        # self.known_face_encodings = [
-        #     self.syl_face_encoding,
-        #     self.zy_face_encoding,
-        #     self.zcy_face_encoding
-        #     ]
-        # self.known_face_names = [
-        #     'song',
-        #     'yi',
-        #     'chong'
-        #     ]
-
-        # self.num = 0
-
-        
 
 
     def img_callback(self, data):
@@ -79,53 +52,86 @@ class face_detect:
         face_locations = face_recognition.face_locations(rgb_frame)
         face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
         
-        # Loop through each face in this frame of video
-        for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-            # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+        if face_encodings:
 
-            name = "Unknown"
+            # Loop through each face in this frame of video
+            for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+                # See if the face is a match for the known face(s)
+                matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
 
-            # If a match was found in known_face_encodings, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = known_face_names[first_match_index]
+                name = "Unknown"
 
-            # Or instead, use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = self.known_face_names[best_match_index]
-            
-            if name == "Song":
-                self.name_msg.data[0] = 1
-            else:
-                self.name_msg.data[0] = 0
+                # Use the known face with the smallest distance to the new face
+                face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = self.known_face_names[best_match_index]
+                
+                # if name == "Song":
+                #     self.name_msg.data[0] = 1
+                # else:
+                #     self.name_msg.data[0] = 0
 
-            if name == 'Yi':
-                self.name_msg.data[1] = 1
-            else:
-                self.name_msg.data[1] = 0
+                # if name == 'Yi':
+                #     self.name_msg.data[1] = 1
+                # else:
+                #     self.name_msg.data[1] = 0
 
-            if name == 'Ma':
-                self.name_msg.data[2] = 1
-            else:
-                self.name_msg.data[2] = 0
-            
-            if name == 'Chong':
-                self.name_msg.data[3] = 1
-            else:
-                self.name_msg.data[3] = 0
-            
-            # Draw a box around the face
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+                # if name == 'Ma':
+                #     self.name_msg.data[2] = 1
+                # else:
+                #     self.name_msg.data[2] = 0
+                
+                # if name == 'Chong':
+                #     self.name_msg.data[3] = 1
+                # else:
+                #     self.name_msg.data[3] = 0
 
-            # Draw a label with a name below the face
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.8, (255, 255, 255), 1)
+                if name == "Mike":
+                    self.face_history[self.count, 0] = 1
+                else:
+                    self.face_history[self.count, 0] = 0
+                
+                if name == "Jack":
+                    self.face_history[self.count, 1] = 1
+                else:
+                    self.face_history[self.count, 1] = 0
+                
+                if name == "Tom":
+                    self.face_history[self.count, 2] = 1
+                else:
+                    self.face_history[self.count, 2] = 0
+
+                if name == "John":
+                    self.face_history[self.count, 3] = 1
+                else:
+                    self.face_history[self.count, 3] = 0
+                
+                # Draw a box around the face
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+                # Draw a label with a name below the face
+                cv2.rectangle(frame, (left, bottom - 30), (right, bottom), (0, 0, 255), cv2.FILLED)
+                font = cv2.FONT_HERSHEY_DUPLEX
+                cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.7, (255, 255, 255), 1)
         
+        else:
+            self.face_history[self.count, 0] = 0
+            self.face_history[self.count, 1] = 0
+            self.face_history[self.count, 2] = 0
+            self.face_history[self.count, 3] = 0
+        
+        self.count = self.count + 1
+        if self.count == 5:
+            self.count = 0
+        
+        stats = np.sum(self.face_history, axis=0)
+        # print(self.face_history)
+        if np.sum(stats) != 0:
+            index = np.argmax(stats)
+            self.name_msg.data[index] = 1
         self.name_pub.publish(self.name_msg)
+
         cv2.imshow('face', frame)
         cv2.waitKey(2)
 
@@ -141,7 +147,7 @@ if __name__ == '__main__':
     syl_path = os.path.join(pkg_path, 'face_img/syl.jpeg')
     zy_path = os.path.join(pkg_path, 'face_img/zy.jpeg')
     mt_path = os.path.join(pkg_path, 'face_img/mt.jpeg')
-    zcy_path = os.path.join(pkg_path, 'face_img/zcy.jpeg')
+    zcy_path = os.path.join(pkg_path, 'face_img/zcy.jpg')
 
     syl_image = face_recognition.load_image_file(syl_path)
     syl_face_encoding = face_recognition.face_encodings(syl_image)[0]
@@ -162,10 +168,10 @@ if __name__ == '__main__':
         zcy_face_encoding
     ]
     known_face_names = [
-        'Song',
-        'Yi',
-        'Ma',
-        'Chong'
+        'Mike',
+        'Jack',
+        'Tom',
+        'John'
     ]
     print(known_face_encodings)
 
